a71840fba087b0c54c8edee9e037f1d91ee6d516
Warkentin Erwin <erwin.warkentin@hahn-softwareentwicklung.com>
2023-03-08 10:44:21 +0100

    Created Project

---
+++ DelegateUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateUIKit
+//
+//  Created by Erwin Warkentin on 08.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
